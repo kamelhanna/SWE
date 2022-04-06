5e31b95d8201346950d5ff4fe1dea4400767144e
kamelhanna <74700713+kamelhanna@users.noreply.github.com>
2022-04-06 08:03:15 +0200

    Add files via upload

--- pr1/scr/Main.java
+++ pr1/scr/Main.java
@@ -1,1 +1,10 @@
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) {
+        Flat flat = new Flat(1, 1, 2000);
+        ArrayList<Flat> flats = new ArrayList<>();
+        flats.add(flat);
+        //CollegeTown collegeTown = new CollegeTown(flats);
+    }
+}
